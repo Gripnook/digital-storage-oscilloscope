8572eb7c3ba5984a4070846589be267b56cef113
Gripnook <XxAndreixX@hotmail.com>
2017-03-12 18:49:39 -0400

    Fixed vga timing generator

--- src/vga_timing_generator.vhd
+++ src/vga_timing_generator.vhd
@@ -1,28 +1,28 @@
 library ieee;
+library lpm;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
-library lpm;
 use lpm.lpm_components.all;
 
 entity vga_timing_generator is
     generic (
-        H_PIXELS : integer   := 800; --horizontal display width in pixels
-        H_PULSE  : integer   := 120; --horizontal sync pulse width in pixels
-        H_FP     : integer   := 56; --horizontal front porch width in pixels
-        H_BP     : integer   := 64; --horizontal back porch width in pixels
-        H_POL    : std_logic := '1'; --horizontal sync pulse polarity (1 = positive, 0 = negative)
-        V_PIXELS : integer   := 600; --vertical display width in rows
-        V_PULSE  : integer   := 6; --vertical sync pulse width in rows
-        V_FP     : integer   := 37;
-        V_BP     : integer   := 23; --vertical back porch width in rows
-        V_POL    : std_logic := '1'
+        H_PIXELS : integer   := 800; -- horizontal display width in pixels
+        H_PULSE  : integer   := 120; -- horizontal sync pulse width in pixels
+        H_BP     : integer   := 56;  -- horizontal back porch width in pixels
+        H_FP     : integer   := 64;  -- horizontal front porch width in pixels
+        H_POL    : std_logic := '0'; -- horizontal sync pulse polarity (1 = positive, 0 = negative)
+        V_PIXELS : integer   := 600; -- vertical display width in rows
+        V_PULSE  : integer   := 6;   -- vertical sync pulse width in rows
+        V_BP     : integer   := 37;  -- vertical back porch width in rows
+        V_FP     : integer   := 23;  -- vertical front porch width in rows
+        V_POL    : std_logic := '0'  -- vertical sync pulse polarity (1 = positive, 0 = negative)
     );
     port (
         clock   : in  std_logic; -- pixel clock at frequency of VGA mode being used
         reset   : in  std_logic; -- asynchronous reset
-        row     : out integer range 0 to H_PIXELS - 1; -- vertical pixel coordinate
-        column  : out integer range 0 to V_PIXELS - 1; -- horizontal pixel coordinate
+        row     : out integer range 0 to V_PIXELS - 1; -- vertical pixel coordinate
+        column  : out integer range 0 to H_PIXELS - 1; -- horizontal pixel coordinate
         hsync   : out std_logic; -- horizontal sync pulse
         vsync   : out std_logic; -- vertical sync pulse
         blank_n : out std_logic -- active low blanking output
@@ -30,54 +30,50 @@ entity vga_timing_generator is
 end vga_timing_generator;
 
 architecture behavior of vga_timing_generator is
-    constant H_PERIOD  :  integer := H_PULSE + H_FP + H_PIXELS + H_BP;  --total number of pixel clocks in a row
-    constant V_PERIOD  :  integer := V_PULSE + V_FP + V_PIXELS + V_BP;  --total number of rows in column
 
+    constant H_PERIOD : integer := H_PULSE + H_BP + H_PIXELS + H_FP;  -- total number of pixel clocks in a row
+    constant V_PERIOD : integer := V_PULSE + V_BP + V_PIXELS + V_FP;  -- total number of rows in column
     constant H_BIT_LENGTH : integer := integer(ceil(log2(real(H_PERIOD))));
     constant V_BIT_LENGTH : integer := integer(ceil(log2(real(V_PERIOD))));
     
-    
-    signal h_count_internal : std_logic_vector(H_BIT_LENGTH - 1 downto 0);
+    signal h_count_internal : std_logic_vector(H_BIT_LENGTH - 1 downto 0) := (others => '0');
     signal h_count : integer range 0 to H_PERIOD - 1;
-    signal v_count_internal : std_logic_vector(V_BIT_LENGTH - 1 downto 0);
+    signal v_count_internal : std_logic_vector(V_BIT_LENGTH - 1 downto 0) := (others => '0');
     signal v_count : integer range 0 to V_PERIOD - 1;
-    signal count_en_v : std_logic;
+    signal v_count_en : std_logic;
     signal h_clr : std_logic;
     signal v_clr : std_logic;
 
 begin
 
-    h_count_internaler : lpm_counter
+    h_counter : lpm_counter
         generic map (LPM_WIDTH => H_BIT_LENGTH)
-        port map (clock => clock, aclr => reset, sclr => h_clr, q => h_count_internal)
+        port map (clock => clock, aclr => reset, sclr => h_clr, q => h_count_internal);
     h_count <= to_integer(unsigned(h_count_internal));
     
-    with h_count select h_clr <=
-        '1' when H_PERIOD - 1,
-        '0' when others;
-
-    v_count_internaler : lpm_counter
+    h_clr <= '1' when (h_count = H_PERIOD - 1)
+        else '0';
+    
+    v_counter : lpm_counter
         generic map (LPM_WIDTH => V_BIT_LENGTH)
-        port map (clock => clock, aclr => reset, sclr => v_clr, q => v_count_internal, cnt_en => count_en_v)
+        port map (clock => clock, aclr => reset, sclr => v_clr, q => v_count_internal, cnt_en => v_count_en);
     v_count <= to_integer(unsigned(v_count_internal));
-
-    with v_count select v_clr <=
-	'1' when V_PERIOD - 1,
-	'0' when others;
-    with h_count select count_en_y <=
-	'1' when H_PERIOD - 1,
-	'0' when others;
+    
+    v_clr <= '1' when (v_count = V_PERIOD - 1)
+        else '0';
+    v_count_en <= h_clr;
   
-    row <= to_unsigned(v_count - (V_PULSE + V_FP), 10) when (v_count >= V_PULSE + V_FP and v_count <= V_PIXELS + V_PULSE + V_FP) 
-           else to_unsigned(599, 10);
-    column <= to_unsigned(h_count - (H_PULSE + H_FP) , 10) when (h_count >= H_PULSE + H_FP and h_count <= H_PIXELS + H_PULSE + H_FP) 
-           else to_unsigned(799, 10);
-    blank_n <= '1' when ((v_count < V_PULSE + V_FP or v_count > V_PIXELS + V_PULSE + V_FP) or ((h_count < H_PULSE + H_FP or h_count > H_PIXELS + H_PULSE + H_FP))) 
-           else '0';
-	
-    hsync <= '0' when (h_count < 120) else
-		'1';
-    vsync <= '0' when (v_count < 6) else
-		'1';
+    row <= (v_count - (V_PULSE + V_BP)) when (v_count >= V_PULSE + V_BP and v_count < V_PULSE + V_BP + V_PIXELS) 
+        else V_PIXELS - 1;
+    column <= (h_count - (H_PULSE + H_BP)) when (h_count >= H_PULSE + H_BP and h_count < H_PULSE + H_BP + H_PIXELS) 
+        else H_PIXELS - 1;
+    blank_n <= '0' when ((v_count < V_PULSE + V_BP or v_count >= V_PULSE + V_BP + V_PIXELS) or
+                        ((h_count < H_PULSE + H_BP or h_count >= H_PULSE + H_BP + H_PIXELS)))
+        else '1';
+    
+    hsync <= H_POL when (h_count < H_PULSE) else
+        not H_POL;
+    vsync <= V_POL when (v_count < V_PULSE) else
+        not V_POL;
 
 end behavior;
--- src/vga_timing_generator_tb.vhd
+++ src/vga_timing_generator_tb.vhd
@@ -1,7 +1,7 @@
 library ieee;
+library std;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_textio.all;
-library std;
 use std.textio.all;
 
 entity vga_timing_generator_tb is
@@ -11,16 +11,16 @@ architecture arch of vga_timing_generator_tb is
 
     component vga_timing_generator is
         generic (
-            H_PIXELS : integer   := 800; --horizontal display width in pixels
-            H_PULSE  : integer   := 120; --horizontal sync pulse width in pixels
-            H_FP     : integer   := 56; --horizontal front porch width in pixels
-            H_BP     : integer   := 64; --horizontal back porch width in pixels
-            H_POL    : std_logic := '1'; --horizontal sync pulse polarity (1 = positive, 0 = negative)
-            V_PIXELS : integer   := 600; --vertical display width in rows
-            V_PULSE  : integer   := 6; --vertical sync pulse width in rows
-            V_FP     : integer   := 37;
-            V_BP     : integer   := 23; --vertical back porch width in rows
-            V_POL    : std_logic := '1'
+            H_PIXELS : integer   := 800; -- horizontal display width in pixels
+            H_PULSE  : integer   := 120; -- horizontal sync pulse width in pixels
+            H_BP     : integer   := 56;  -- horizontal back porch width in pixels
+            H_FP     : integer   := 64;  -- horizontal front porch width in pixels
+            H_POL    : std_logic := '0'; -- horizontal sync pulse polarity (1 = positive, 0 = negative)
+            V_PIXELS : integer   := 600; -- vertical display width in rows
+            V_PULSE  : integer   := 6;   -- vertical sync pulse width in rows
+            V_BP     : integer   := 37;  -- vertical back porch width in rows
+            V_FP     : integer   := 23;  -- vertical front porch width in rows
+            V_POL    : std_logic := '0'  -- vertical sync pulse polarity (1 = positive, 0 = negative)
         );
         port (
             clock   : in  std_logic; -- pixel clock at frequency of VGA mode being used
@@ -78,9 +78,9 @@ begin
             if (column < 100) then
                 rgb <= x"FFFFFF";
             elsif (column < 200) then
-                rgb <= x"00FFFF";
+                rgb <= x"FFFF00";
             elsif (column < 300) then
-                rgb <= x"007FFF";
+                rgb <= x"00FFFF";
             elsif (column < 400) then
                 rgb <= x"00FF00";
             elsif (column < 500) then
@@ -122,6 +122,7 @@ begin
     test_process : process
     begin
         reset <= '1';
+        wait until rising_edge(clock);
         reset <= '0';
         wait;
     end process;
