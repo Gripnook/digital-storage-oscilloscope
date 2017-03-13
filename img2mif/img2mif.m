ff8844a6a2a38b3a5deaa4f37b4381dd50d1f810
Gripnook <XxAndreixX@hotmail.com>
2017-03-12 20:47:45 -0400

    Fixed img to mif conversion

--- img2mif/img2mif.m
+++ img2mif/img2mif.m
@@ -1,10 +1,9 @@
-src = imread('../src/background.bmp');
+src = uint32(imread('../src/background.bmp'));
 data = src(:,:,1)*65536 + src(:,:,2)*256 + src(:,:,3);
 [m,n] = size(data);
 
 N = m*n;
 word_len = 24;
-data = reshape(data, 1, N);
 
 fid = fopen('../src/background.mif', 'w');
 fprintf(fid, 'DEPTH=%d;\n', N);
